b127c5a4541c4222b46122742566d5d3e9aa9633
ddecicco330web <ddecicco330@gmail.com>
2024-07-25 18:41:23 -0400

    Implement Header

--- src/components/header-view/header-view.js
+++ src/components/header-view/header-view.js
@@ -1,7 +1,11 @@
 const HeaderView = () => {
   return (
     <header className="header" id="header">
-      Header
+      <h1 className="heading-1">Hello!</h1>
+      <h2 className="heading-2">
+        <span>I'm</span> Dominic DeCicco
+      </h2>
+      <h3 className="heading-3">Fullstack Developer</h3>
     </header>
   );
 };
