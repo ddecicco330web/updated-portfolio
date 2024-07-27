51826c8c4979a304444bb9d08a7790303bc4e7ad
ddecicco330web <ddecicco330@gmail.com>
2024-07-27 11:45:44 -0400

    Implement Projects View

---
+++ src/components/project-card-view/project-card-view.js
@@ -0,0 +1,47 @@
+const ProjectCardView = ({
+  name,
+  description,
+  index,
+  gitHubLink,
+  siteLink
+}) => {
+  return (
+    <div className={`project-card project-card--${index}`}>
+      <div className="project-card__info">
+        <div className="project-card__links">
+          <a
+            className="icon-btn icon-btn--project-link"
+            href={gitHubLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <svg>
+              <use
+                xlinkHref={process.env.PUBLIC_URL + 'sprites.svg#icon-github'}
+              ></use>
+            </svg>
+          </a>
+
+          {siteLink && (
+            <a
+              className="icon-btn icon-btn--project-link"
+              href={siteLink}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <svg>
+                <use
+                  xlinkHref={process.env.PUBLIC_URL + 'sprites.svg#icon-link'}
+                ></use>
+              </svg>
+            </a>
+          )}
+        </div>
+        <h4 className="heading-4">{name}</h4>
+        <p className="paragraph paragraph--project-text">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCardView;
--- src/components/projects-view/projects-view.js
+++ src/components/projects-view/projects-view.js
@@ -1,7 +1,36 @@
+import ProjectCardView from '../project-card-view/project-card-view';
+
 const ProjectsView = () => {
   return (
     <section className="projects" id="projects">
-      Projects
+      <div className="projects__title-row">
+        <h2 className="heading-2 heading-2--section-title">Projects</h2>
+      </div>
+      <div className="projects__project-list">
+        <ProjectCardView
+          index={1}
+          name="Recipes"
+          description="Django, Python, AWS S3"
+          gitHubLink="https://github.com/ddecicco330web/django-recipe-app"
+          siteLink="https://recipes330-b394d7d440dc.herokuapp.com/"
+        />
+
+        <ProjectCardView
+          index={2}
+          name="MyFlix"
+          description="React, React Bootstrap, JSX, Node"
+          gitHubLink="https://github.com/ddecicco330web/myFlix-client"
+          siteLink="https://myflix330.netlify.app/login"
+        />
+
+        <ProjectCardView
+          index={3}
+          name="Chat"
+          description="React Native, JS, Firebase"
+          gitHubLink="https://github.com/ddecicco330web/chat"
+          siteLink={null}
+        />
+      </div>
     </section>
   );
 };
