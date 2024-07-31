d3ed1155d1d60c80d348405163ce4a8a22b4c3d7
ddecicco330web <ddecicco330@gmail.com>
2024-07-31 18:24:48 -0400

    Add handleClick function

--- src/components/nav-modal-view/nav-modal-view.js
+++ src/components/nav-modal-view/nav-modal-view.js
@@ -1,56 +1,31 @@
 const NavModalView = ({ showModal, setShowModal }) => {
+  const handleClick = () => {
+    setShowModal(false);
+  };
+
   return (
     <nav className={`nav-modal ${showModal && 'nav-modal--visible'}`}>
-      <button
-        className="close-btn close-btn--nav-modal"
-        onClick={() => {
-          setShowModal(false);
-        }}
-      >
+      <button className="close-btn close-btn--nav-modal" onClick={handleClick}>
         X
       </button>
       <ul className="nav-modal__list">
         <li className="nav-modal__item">
-          <a
-            href="#header"
-            className="nav-modal__link"
-            onClick={() => {
-              setShowModal(false);
-            }}
-          >
+          <a href="#header" className="nav-modal__link" onClick={handleClick}>
             Home
           </a>
         </li>
         <li className="nav-modal__item">
-          <a
-            href="#about"
-            className="nav-modal__link"
-            onClick={() => {
-              setShowModal(false);
-            }}
-          >
+          <a href="#about" className="nav-modal__link" onClick={handleClick}>
             About
           </a>
         </li>
         <li className="nav-modal__item">
-          <a
-            href="#projects"
-            className="nav-modal__link"
-            onClick={() => {
-              setShowModal(false);
-            }}
-          >
+          <a href="#projects" className="nav-modal__link" onClick={handleClick}>
             Projects
           </a>
         </li>
         <li className="nav-modal__item">
-          <a
-            href="#contact"
-            className="nav-modal__link"
-            onClick={() => {
-              setShowModal(false);
-            }}
-          >
+          <a href="#contact" className="nav-modal__link" onClick={handleClick}>
             Contact
           </a>
         </li>
